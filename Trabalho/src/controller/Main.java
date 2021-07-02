88f151f26064079f62f55d456c80a07249249856
Brenda Paetzoldt Silva <brendapaetzoldt@yahoo.com.br>
2021-07-01 21:58:05 -0300

    Primeira tela

---
+++ Trabalho/src/controller/Main.java
@@ -0,0 +1,14 @@
+package controller;
+
+import view.TelaInicial;
+
+/**
+ *
+ * @author Brenda Paetzoldt e Robson de Jesus
+ */
+public class Main {
+    public static void main(String[] args) {
+        TelaInicial ti = new TelaInicial();
+        ti.setVisible(true);
+    }
+}
